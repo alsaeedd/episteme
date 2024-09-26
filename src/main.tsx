e7b8e8789521b7ce5aa48252014bce3fe72af553
Mohammed Alsaeed <saeedalsaeedbusiness@gmail.com>
2024-09-26 18:56:54 +0300

    Adjusts styling on sign in page

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,15 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import {BrowserRouter} from "react-router-dom";
 import { Amplify } from 'aws-amplify';
-import { Authenticator } from '@aws-amplify/ui-react';
+import {
+    Authenticator,
+    useTheme,
+    createTheme,
+    ThemeProvider,
+    Theme,
+    View,
+    Heading
+} from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
 import CustomSignOutButton from "./CustomSignOutButton.tsx";
 
@@ -34,19 +42,45 @@ Amplify.configure({
     },
 });
 
+const components = {
+    Header() {
+        const { tokens } = useTheme();
+
+        return (
+            <View textAlign="center" padding={tokens.space.large}>
+                <h1 className={'mb-4 justify-content-center align-items-center'}>Episteme</h1>
+            </View>
+        );
+    },
+    // SignIn: {
+    //     Header() {
+    //         const { tokens } = useTheme();
+    //
+    //         return (
+    //             <Heading
+    //                 padding={`${tokens.space.xl} 0 0 ${tokens.space.xl}`}
+    //                 level={3}
+    //             >
+    //                 Sign in to your account
+    //             </Heading>
+    //         );
+    //     },
+}
+
+
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-          <Authenticator loginMechanisms={['email']}>
-          {({ signOut}) => (
-              <>
-                  <main>
-                      <CustomSignOutButton signOut={signOut} />
-                  </main>
-                  <BrowserRouter>
-                      <App/>
-                  </BrowserRouter>
-              </>
-          )}
-          </Authenticator>
-      </StrictMode>
+    <StrictMode>
+            <Authenticator loginMechanisms={['email']} components={components}>
+                {({signOut}) => (
+                    <>
+                        <main>
+                            <CustomSignOutButton signOut={signOut}/>
+                        </main>
+                        <BrowserRouter>
+                            <App/>
+                        </BrowserRouter>
+                    </>
+                )}
+            </Authenticator>
+    </StrictMode>
 )
